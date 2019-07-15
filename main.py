15cdb2334e767556fbfcd3a512ddad173f5aa804
Vitold-sys <52232795+Vitold-sys@users.noreply.github.com>
2019-07-15 20:58:14 +0300

    Update main.py

--- main.py
+++ main.py
@@ -8,11 +8,9 @@ import random
 smiles = ['🤣', '😕', '😊', '😀', '😌', '👄', '👎', '👅', '👀', '😊', '🧐', '🤓', '🙃', '💍', '💩']
 animals = ['🐶', '🐴', '🐗', '🦓', '🦒', '🦌', '🐢', '🐍', '🐻', '🐑', '🐖', '🐓', '🐇', '🐀', '🐒']
 
-bot = telebot.TeleBot("783427832:AAGItYQLE6FIbjbLvAfSqBiBKOK-EQ6apsU")
-
 def telegram_bot_sendtext(bot_message):
-    bot_token = '783427832:AAGItYQLE6FIbjbLvAfSqBiBKOK-EQ6apsU'
-    bot_chatID = '-384696558'
+    bot_token = 'Token'
+    bot_chatID = 'Chatid'
     send_text = 'https://api.telegram.org/bot' + bot_token + '/sendMessage?chat_id=' + bot_chatID +\
                 '&parse_mode=Markdown&text=' + bot_message
 
